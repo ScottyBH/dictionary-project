999733bec6f9ffd076826976b1f39faab745866b
ScottyBH <112198546+ScottyBH@users.noreply.github.com>
2023-04-21 14:41:34 -0700

    Centered text

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+    <div className="meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
