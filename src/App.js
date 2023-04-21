09be984063910ebabace9801fd9c7f28c6f6ec3a
ScottyBH <112198546+ScottyBH@users.noreply.github.com>
2023-04-21 13:27:43 -0700

    Added logo and created form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Meghan Hardeman</footer>
     </div>
   );
 }
